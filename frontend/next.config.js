b5309c40b75eb7bbcace4d97b26710ba1f96606d
hub-for-aftertechnologies <aftertechnologies@hotmail.com>
2024-09-09 23:28:05 +0300

    Adding forum

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  generateBuildId: async () => {
-      // This could be anything, using the latest git hash
-      return process.env.GIT_HASH || "dev-build"
-  },
   images: {
       remotePatterns: [
           {
